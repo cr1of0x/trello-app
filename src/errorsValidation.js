b194d08adb0f8bccbd3a4329e89a8c4af4a06812
criofox <imbaac@gmail.com>
2022-02-11 12:10:20 +0300

    feature/TC-1 soft commit

--- src/errorsValidation.js
+++ src/errorsValidation.js
@@ -1,5 +1,5 @@
 export const errorsValidation = (error, setErrors) => {
-  if (error.request.status === 422) {
+  if (error.request.status === 400) {
     let errorsArray = error.response.data.error.details;
     let errors = errorsArray.reduce(
       (obj, item) => ((obj[item.context.key] = item.message), obj),
