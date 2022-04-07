54977a0b4275ba7efd7b3fda74c129cd94278b00
criofox <imbaac@gmail.com>
2022-04-07 12:34:51 +0300

    deploy fix

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,10 @@
 export const PUBLIC_ROUTES = {
-  login: "/trello-app//login",
-  confirmemail: "/trello-app//confirmemail",
-  registration: "/trello-app//registration",
+  login: "/trello-app/login",
+  confirmemail: "/trello-app/confirmemail",
+  registration: "/trello-app/registration",
 };
 export const PRIVATE_ROUTES = {
   homepage: "/trello-app",
-  dashboard: "/trello-app//dashboard",
+  dashboard: "/trello-app/dashboard",
   dashboardDetails: "trello-app/b/:pathId",
 };
